c7355601a32e22ff614190986d30366db125e5be
Mihancheggg <schutow.mihail@yandex.ru>
2022-04-06 10:14:40 +0300

    one more task

--- tasks.js
+++ tasks.js
@@ -5,3 +5,17 @@ function go(age, acc){
         console.log("Go");
     else console.log("Stop");
     };
+
+// p. 82
+// Нужно выдернуть 123
+var myCrazyObject = {
+    "name": "Нелепый объект",
+    "Some array": [7, 9, {
+        purpose: "Путаница",
+        number: 123
+    },
+    3,3],
+    "random animal": "Банановая акула"
+};
+
+alert(myCrazyObject["Some array"][2].number);
