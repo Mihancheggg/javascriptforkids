3e4dd655e77a3f41e9a266197fa5c830c1b6ca77
Mihancheggg <schutow.mihail@yandex.ru>
2022-05-05 12:04:54 +0300

    features added

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 //Создаем массив со словами
-var words = ["компьютер", "монитор", "клавиатура", "мышь", "колонки", "процессор", "программа"];
+var words = ["компьютер", "монитор", "клавиатура", "мышь", "колонки", "процессор", "программа", "видеокарта", "микрофон", "браузер", "стол"];
 
 //Выбираем случайное слово
 var word = words[Math.floor(Math.random() * words.length)];
@@ -10,16 +10,18 @@ for (var i = 0; i < word.length; i++){
     answerArray[i] = "_"
 }
 var remainingLetters = word.length;
+var tries = 6;
+var target = false;
 
 //Игровой цикл
-while(remainingLetters > 0){
+while(remainingLetters > 0 && tries > 0){
 
     //Показываем состояние игры
     alert(answerArray.join(" "));
 
 
     //Запрашиваем вариант ответа
-    var guess = prompt("Угадайте букву, или нажмите Отмена для выхода из игры");
+    var guess = prompt("Угадайте букву, или нажмите Отмена для выхода из игры").toLowerCase();
     if (guess === null){
 
         //Выходим из игрового цикла
@@ -30,13 +32,17 @@ while(remainingLetters > 0){
 
         //Обновляем состояние игры
         for(var j = 0; j < word.length; j++){
-            if(word[j] === guess){
+            if(word[j] === guess && answerArray[j] === "_"){
                 answerArray[j] = guess;
                 remainingLetters--;
+                target = true;
+            } else if (word[j] === guess && answerArray[j] === guess){
+                alert("Вы уже угадали эту букву");
             }
         }
     }
-
+    console.log(tries);
+    console.log(remainingLetters);
     //Конец игрового цикла
 };
 
